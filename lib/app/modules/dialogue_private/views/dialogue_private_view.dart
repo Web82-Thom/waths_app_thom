9586ce49cc94cf84880c8382a3859a0a2a11fa61
Thomas <lefoux82@gmail.com>
2021-06-07 12:22:36 +0200

    create textField in screen dialogue private

--- lib/app/modules/dialogue_private/views/dialogue_private_view.dart
+++ lib/app/modules/dialogue_private/views/dialogue_private_view.dart
@@ -11,158 +11,243 @@ import 'package:whats_app_thom/app/routes/app_pages.dart';
 import '../controllers/dialogue_private_controller.dart';
 
 class DialoguePrivateView extends GetView<DialoguePrivateController> {
-  final DialoguePrivateController dialoguePrivateController = Get.put(DialoguePrivateController(),);
-  final DialoguesController dialoguesController = Get.put(DialoguesController(),);
-  
+  final DialoguePrivateController dialoguePrivateController = Get.put(
+    DialoguePrivateController(),
+  );
+  final DialoguesController dialoguesController = Get.put(
+    DialoguesController(),
+  );
+
   int indexPassed = int.parse("${Get.parameters['indexPassed']}");
- 
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leadingWidth: 30,
-        titleSpacing: 2,
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            CircleAvatar(child: SvgPicture.asset(
-              dialoguesController.dialogues[indexPassed].isGroup ? 
-              'assets/icons/groups.svg':
-                "assets/icons/person.svg",
-              
-              color: Colors.white,
-              height: 30.0,
-              width: 30.0,
-            ),radius:20.0, backgroundColor: Colors.blueGrey,),
-            SizedBox(width: 3.0,),
-            InkWell(
-              onTap: () {
-                print('inkWell');
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          leadingWidth: 30,
+          titleSpacing: 2,
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              CircleAvatar(
+                child: SvgPicture.asset(
+                  dialoguesController.dialogues[indexPassed].isGroup
+                      ? 'assets/icons/groups.svg'
+                      : "assets/icons/person.svg",
+                  color: Colors.white,
+                  height: 30.0,
+                  width: 30.0,
+                ),
+                radius: 20.0,
+                backgroundColor: Colors.blueGrey,
+              ),
+              SizedBox(
+                width: 3.0,
+              ),
+              InkWell(
+                onTap: () {
+                  print('inkWell');
+                },
+                child: Container(
+                  margin: EdgeInsets.all(5.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        dialoguesController.dialogues[indexPassed].name,
+                        style: TextStyle(
+                          fontSize: 15.5,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        'Vu aujourd\'hui à ' +
+                            dialoguesController.dialogues[indexPassed].time,
+                        style: TextStyle(
+                          fontSize: 8.0,
+                          fontWeight: FontWeight.normal,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.videocam),
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: Icon(Icons.call),
+              onPressed: () {},
+            ),
+            PopupMenuButton<String>(
+              onSelected: (value) {
+                print(value);
               },
-              child: Container(
-                margin: EdgeInsets.all(5.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                     dialoguesController.dialogues[indexPassed].name, 
-                      style: TextStyle(
-                        fontSize: 15.5,
-                        fontWeight: FontWeight.bold,
+              itemBuilder: (BuildContext ctx) {
+                return [
+                  PopupMenuItem(
+                    child: Text('Afficher contact'),
+                    value: 'Afficher contact',
+                  ),
+                  PopupMenuItem(
+                    child: Text('Médias, liens et documents'),
+                    value: 'Médias, liens et documents',
+                  ),
+                  PopupMenuItem(
+                    child: Text('Rechercher'),
+                    value: 'Rechercher',
+                  ),
+                  PopupMenuItem(
+                    child: Text('Notifications en mode solencieux'),
+                    value: 'Notifications en mode solencieux',
+                  ),
+                  PopupMenuItem(
+                    child: Text('Fond d\'écran'),
+                    value: 'Fond d\'écran',
+                  ),
+                  PopupMenuItem(
+                    child: InkWell(
+                      onTap: () {
+                        PopupMenuButton<String>(
+                          onSelected: (value) {
+                            print(value);
+                          },
+                          itemBuilder: (BuildContext ctx) {
+                            return [
+                              PopupMenuItem(
+                                child: Text('Afficher contact'),
+                                value: 'Afficher contact',
+                              ),
+                              PopupMenuItem(
+                                child: Text('Médias, liens et documents'),
+                                value: 'Médias, liens et documents',
+                              ),
+                              PopupMenuItem(
+                                child: Text('Rechercher'),
+                                value: 'Rechercher',
+                              ),
+                              PopupMenuItem(
+                                child: Text('Notifications en mode solencieux'),
+                                value: 'Notifications en mode solencieux',
+                              ),
+                              PopupMenuItem(
+                                child: Text('Fond d\'écran'),
+                                value: 'Fond d\'écran',
+                              ),
+                              PopupMenuItem(
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text('Plus'),
+                                    IconButton(
+                                        onPressed: () {
+                                          print('sous menu');
+                                        },
+                                        icon: Icon(
+                                          Icons.arrow_right,
+                                          color: Colors.grey,
+                                        ))
+                                  ],
+                                ),
+                                value: 'Plus',
+                              ),
+                            ];
+                          },
+                        );
+                      },
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text('Plus'),
+                          Icon(
+                            Icons.arrow_right,
+                            color: Colors.grey,
+                          ),
+                        ],
                       ),
                     ),
-                    Text(
-                      'Vu aujourd\'hui à ' + dialoguesController.dialogues[indexPassed].time, 
-                      style: TextStyle(
-                        fontSize: 8.0,
-                        fontWeight: FontWeight.normal,
+                    value: 'Plus',
+                  ),
+                ];
+              },
+            ),
+          ],
+        ),
+        body: Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          child: Stack(
+            children: [
+              ListView(),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Row(
+                  children: [
+                    Container(
+                      width: MediaQuery.of(context).size.width - 55,
+                      child: Card(
+                        margin: EdgeInsets.only(
+                          right: 2,
+                          left: 2,
+                          bottom: 8,
+                        ),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(25)),
+                        child: TextFormField(
+                          textAlignVertical: TextAlignVertical.center,
+                          keyboardType: TextInputType.multiline,
+                          maxLines: 5,
+                          minLines: 1,
+                          decoration: InputDecoration(
+                              border: InputBorder.none,
+                              hintText: 'Votre méssage',
+                              prefixIcon: IconButton(
+                                icon: Icon(Icons.emoji_emotions),
+                                onPressed: () {},
+                              ),
+                              suffixIcon: Row(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  IconButton(
+                                    onPressed: () {},
+                                    icon: Icon(
+                                      Icons.attach_file,
+                                    ),
+                                  ),
+                                  IconButton(
+                                    onPressed: () {},
+                                    icon: Icon(
+                                      Icons.camera_alt,
+                                    ),
+                                  ),
+                                ],
+                              )),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 8,),
+                      child: CircleAvatar(
+                        backgroundColor: Theme.of(context).primaryColor,
+                        radius: 25,
+                        child: IconButton(
+                          icon: Icon(Icons.mic, color: Colors.white,),
+                          onPressed: () {},
+                        ),
                       ),
                     ),
                   ],
                 ),
               ),
-            ),
-          ],
-        ),
-        actions : [
-          IconButton(icon: Icon(Icons.videocam), onPressed: () {},),
-          IconButton(icon: Icon(Icons.call), onPressed: () {},),
-          PopupMenuButton<String>(
-            onSelected: (value) {
-              print(value);
-            },
-            itemBuilder: (BuildContext ctx) {
-              return 
-              [
-                PopupMenuItem(
-                  child: Text('Afficher contact'),
-                  value: 'Afficher contact',
-                ),
-                PopupMenuItem(
-                  child: Text('Médias, liens et documents'),
-                  value: 'Médias, liens et documents',
-                ),
-                PopupMenuItem(
-                  child: Text('Rechercher'),
-                  value: 'Rechercher',
-                ),
-                PopupMenuItem(
-                  child: Text('Notifications en mode solencieux'),
-                  value: 'Notifications en mode solencieux',
-                ),
-                PopupMenuItem(
-                  child: Text('Fond d\'écran'),
-                  value: 'Fond d\'écran',
-                ),
-                PopupMenuItem(
-                  child: InkWell(
-                    onTap: () {
-                      PopupMenuButton<String>(
-                        onSelected: (value) {
-                          print(value);
-                        },
-                        itemBuilder: (BuildContext ctx) {
-                          return 
-                          [
-                            PopupMenuItem(
-                              child: Text('Afficher contact'),
-                              value: 'Afficher contact',
-                            ),
-                            PopupMenuItem(
-                              child: Text('Médias, liens et documents'),
-                              value: 'Médias, liens et documents',
-                            ),
-                            PopupMenuItem(
-                              child: Text('Rechercher'),
-                              value: 'Rechercher',
-                            ),
-                            PopupMenuItem(
-                              child: Text('Notifications en mode solencieux'),
-                              value: 'Notifications en mode solencieux',
-                            ),
-                            PopupMenuItem(
-                              child: Text('Fond d\'écran'),
-                              value: 'Fond d\'écran',
-                            ),
-                            PopupMenuItem(
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Text('Plus'),
-                                  IconButton(
-                                    onPressed: () {
-                                      print('sous menu');
-                                    }, icon: Icon(Icons.arrow_right, color: Colors.grey,))
-                                ],
-                              ),
-                              value: 'Plus',
-                            ),
-                          ];
-                        },
-                      );
-                    },
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text('Plus'),
-                        Icon(Icons.arrow_right, color: Colors.grey,),
-                      ],
-                    ),
-                  ),
-                  value: 'Plus',
-                ),
-              ];
-            },
+            ],
           ),
-        ],
-      ),
-      body: Center(
-        child: Text(
-          'DialoguePrivateView is working',
-          style: TextStyle(fontSize: 20),
-        ),
-      ),
-    );
+        ));
   }
 }
